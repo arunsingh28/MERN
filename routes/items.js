45f4faa986c0bce822a5faa0f27325cf90f575ac
Arun Singh <arunsingh28aug.as@gmail.com>
2021-05-18 22:02:08 +0530

    REST API

--- routes/items.js
+++ routes/items.js
@@ -1,5 +1,6 @@
 const express = require('express')
 
+
 const router = express.Router()
 
 const Item = require('../Model/item')
@@ -11,6 +12,29 @@ router.get('/', (req, res) => {
         .then(items => res.json(items))
 })
 
+
+router.post('/', async(req, res) => {
+   try {
+    const newItem = await new Item({
+        name : req.body.name
+    })
+    newItem.save()
+    .then(item => res.json(item)) 
+   } catch (error) {
+      return res.json({error})
+   }
+})
+
+
+router.delete('/:id',async(req,res)=>{
+    try {
+        const _id = req.params.id
+        await Item.findById(_id)
+        .then(item => Item.remove().then(()=> res.json({success:true})))
+    } catch (error) {
+        return res.json({error})
+    }
+})
 
 
 
