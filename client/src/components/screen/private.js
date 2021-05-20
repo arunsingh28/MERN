49b2d87096c7f01f7daff54b321c42c418fc3f02
Arun Singh <arunsingh28aug.as@gmail.com>
2021-05-20 09:54:12 +0530

    login

--- client/src/components/screen/private.js
+++ client/src/components/screen/private.js
@@ -1,13 +1,41 @@
-import React,{useEffect} from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
 
-const Private = () => {
-    useEffect(()=>{
+
+const Private = ({ history }) => {
+    const [error, setError] = useState('')
+    const [privateDate, setPrivateData] = useState('')
+
+    useEffect(() => {
         document.title = 'Private route'
-    })
+
+        if (localStorage.getItem('authToken')) {
+            history.push('/login')
+        }
+        const fetchPrivateData = async () => {
+            const config = {
+                header: {
+                    'Content-Type': 'application/json',
+                    Authorization: `Bearer ${localStorage.getItem('authToken')}`
+                }
+            }
+
+            try {
+                const { data } = await axios.get('/api/private', config)
+                setPrivateData(data.data)
+            } catch (error) {
+                localStorage.removeItem('authToken')
+                setError('You are not Authorize please login first.')
+            }
+        }
+        fetchPrivateData()
+    }, [history])
+
+
     return (
-        <div>
-            private
-        </div>
+        <>
+            {error ? { error } : { privateDate } }
+        </>
     )
 }
 
